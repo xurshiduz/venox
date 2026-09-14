d12f0a2d5f9efcba47aeafc56b2a5b7c08860828
diyorbek1118 <159356339+diyorbek1118@users.noreply.github.com>
2026-09-14 13:15:46 +0500

    Add one-time transfer migration runner

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::post('/search_barcode', 'Backend\ProductController@api_search_barcode');
 
@@ -19,3 +20,13 @@ Route::post('/factory-transfer/request', 'Api\CheckinController@apiCheckinSave')
     ->withoutMiddleware(['throttle:api']);
 Route::get('/lidaz-ledger/supplier-return/{code}', 'Api\SupplierReturnRequestController@show')->where('code', '[0-9a-fA-F-]{36}')->withoutMiddleware(['throttle:api']);
 Route::post('/lidaz-ledger/supplier-return/{code}/accepted', 'Api\SupplierReturnRequestController@accepted')->where('code', '[0-9a-fA-F-]{36}')->withoutMiddleware(['throttle:api']);
+
+Route::post('/ops/6275a32d9e7a5e894a48a8f13f138d8022b6afd24b84559b/migrate-transfer-flow', function () {
+    foreach ([
+        'database/migrations/2026_09_14_120000_add_factory_transfer_source_to_checkins_table.php',
+        'database/migrations/2026_09_14_121000_create_supplier_return_requests_tables.php',
+    ] as $path) {
+        Artisan::call('migrate', ['--path' => $path, '--force' => true]);
+    }
+    return response()->json(['ok' => true, 'output' => Artisan::output()]);
+})->withoutMiddleware(['throttle:api']);
