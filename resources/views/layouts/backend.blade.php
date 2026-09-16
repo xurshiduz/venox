dbb3875d6f221facb97ba8c80f0d169a8b74d15b
diyorbek1118 <159356339+diyorbek1118@users.noreply.github.com>
2026-09-16 14:57:38 +0500

    Unify and refine dashboard visual design

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -11,7 +11,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="/backend/assets/css/dashlite.css?ver=3.0.3">
     <link id="skin-default" rel="stylesheet" href="/backend/assets/css/theme.css?ver=3.0.3">
-    <link rel="stylesheet" href="/backend/css/dashboard-ui.css?v=1.0.8">
+    <link rel="stylesheet" href="/backend/css/dashboard-ui.css?v=1.1.0">
     
     @yield('css')
 </head>
